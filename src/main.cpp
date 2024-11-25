28176b93ea3de799474b2a47c84626e04f696f24
unknown <odoul28@gmail.com>
2024-11-24 17:59:46 -0800

    Add sprite data

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduboy2.h>
+#include <ArduboyTones.h>
+#include <data.h>
 
 Arduboy2 arduboy;
 
 void setup() {
     arduboy.begin();
 }
+
 void loop() {
     if (!arduboy.nextFrame()) {
         return;
